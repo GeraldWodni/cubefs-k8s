79090d9060b746c0d8a07a1468b9324d1587cc3c
Gerald Wodni <gerald.wodni@gmail.com>
2026-05-13 13:13:14 +0200

    server command added, building on github

--- docker/server/src/startup.sh
+++ docker/server/src/startup.sh
@@ -7,5 +7,5 @@ env | /bin/env2json.awk > /etc/config-live.json
 jq -s '.[0] * .[1]' /etc/config-base.json /etc/config-live.json > /etc/config.json
 
 # run server
-/bin/c
+/bin/cfs-server -c /etc/config.json
 
