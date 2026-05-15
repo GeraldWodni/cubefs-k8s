07d8ea4889e6876127491ef8c19c2934d10a3201
Gerald Wodni <gerald.wodni@gmail.com>
2026-05-15 18:34:22 +0200

    server startup: debugging prints added

--- docker/server/src/startup.sh
+++ docker/server/src/startup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+echo "Startup"
+echo "  Writing live-config"
 
 # get configuration from environment
 env | /bin/env2json.awk > /etc/config-live.json
 
 if [ -f /etc/layout.json ]; then
+    echo "  Writing config (w/ layout)"
     # big configuration with layout
     # get node configuration
     cat /etc/layout.json | jq ".[\"$NODE_NAME\"]" > /etc/config-node.json
@@ -11,9 +14,14 @@ if [ -f /etc/layout.json ]; then
     # merge configurations
     jq -s '.[0] * .[1] * .[2]' /etc/config-base.json /etc/config-node.json /etc/config-live.json > /etc/config.json
 else
+    echo "  Writing config (w/o layout)"
     # small configuration
     # merge configurations
     jq -s '.[0] * .[1]' /etc/config-base.json /etc/config-live.json > /etc/config.json
 fi
 
+echo "Final config:"
+cat /etc/config.json | jq
+
+echo "Config finished, starting cfs"
 /bin/cfs-server -redirect-std -f -c /etc/config.json
